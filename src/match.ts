905b4481c828b052d67e35f3332b7b9b816c768a
Tushar Mathur <tusharmath@gmail.com>
2018-06-27 23:49:44 +0530

    fix(tsc): fix typings for curried function

--- src/match.ts
+++ src/match.ts
@@ -1,9 +1,13 @@
-import {curry2} from 'ts-curry'
+import {CurriedFunction2, curry2} from 'ts-curry'
 export type MatchActionSpec = {
   [key: string]: {(value: any): any}
 }
 
-export const match = curry2(
+export const match: CurriedFunction2<
+  (t: any) => any,
+  MatchActionSpec,
+  any
+> = curry2(
   (base: (t: any) => any, spec: MatchActionSpec) => (action: Action<any>) =>
     spec[action.type] ? spec[action.type](action.value) : base(action.value)
 )
